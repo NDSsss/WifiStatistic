1c3b22a20236f814e0df04b665e608d08d8e3933
NDS <dddddd1997@gmail.com>
2018-11-04 01:08:58 +0200

    testing git

--- app/src/main/java/com/example/wifistatistic/Classes/Measurement.java
+++ app/src/main/java/com/example/wifistatistic/Classes/Measurement.java
@@ -20,7 +20,10 @@ public class Measurement {
 
     public boolean setPoints(String data){
         WiFiPoint point;
-        String[] pointsString = data.split(FieldConverter.getString(R.string.devider));
+        char[] rejex =new char[1];
+        rejex[0] = ((char)140);
+        String rejexStr = rejex.toString();
+        String[] pointsString = data.split(rejexStr);
         for(int i = 0; i<pointsString.length-1;i++){
             point = new WiFiPoint();
             if(point.setPoint(pointsString[i])) {
